4d81c56418c496211ad149f21352b057c47f6d19
Davide Bernardi <127590023+DavideBernardii@users.noreply.github.com>
2023-05-06 09:26:49 +0200

    Add files via upload

---
+++ Isogramma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+
+        word = word.ToLower();
+
+        for(int i=0;i<word.Length;i++)
+        for(int n = 0; n < word.Length; n++)
+        if((word[i] == word[n]) && (i != n) && (word[i] != '') && (word[i] != '-'))
+
+        return false;
+
+        return true;
+    }
+
+}
